e4d0d4fd309f60d08561dda8a77db15ca40e6415
Iago Breno Alves do Carmo Araujo <ibaraujo@radius.ime.usp.br>
2018-08-28 18:58:05 -0300

    Update the render function for the Game component

--- index.js
+++ index.js
@@ -23,7 +23,15 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div>Game</div>
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
     );
   }
 }

8eac41b756767c562bd8d45fe3e595bd3347e63e
Iago Breno Alves do Carmo Araujo <ibaraujo@poshul.ime.usp.br>
2018-08-30 18:58:10 -0300

    Update the project hierarchy

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
         {/* TODO */}
       </button>
-	);
+    );
   }
 }
 
@@ -12,24 +15,28 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square />;
   }
-  
+
   render() {
+    const status = 'Next player: X';
+
     return (
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
@@ -50,6 +57,8 @@ class Game extends React.Component {
     );
   }
 }
+
+// ========================================
 
 ReactDOM.render(
   <Game />,
